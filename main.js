339ba4fa59d5133cff06ab934f76bd16018193fa
Scott Cormier <chicormier@gmail.com>
2025-01-26 16:50:51 -0600

    added feral hog data

--- main.js
+++ main.js
@@ -3,11 +3,22 @@ const ctx = document.getElementById('myChart');
 new Chart(ctx, {
   type: 'bar',
   data: {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    labels: [
+      'Texas',
+      'Georgia',
+      'Florida',
+      'Mississippi',
+      'Oklahoma',
+      'Arkansas',
+      'North Carolina',
+      'Louisiana',
+      'Alabama',
+      'California',
+    ],
     datasets: [
       {
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
+        label: 'Reported Sightings',
+        data: [2425, 1377, 1479, 731, 665, 657, 655, 613, 590, 564],
         borderWidth: 1,
       },
     ],
@@ -27,11 +38,25 @@ const ctx2 = document.getElementById('myChart2');
 new Chart(ctx2, {
   type: 'line',
   data: {
-    labels: ['Rd', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange', 'Green'],
+    labels: [
+      'Texas',
+      'Georgia',
+      'Florida',
+      'Mississippi',
+      'Oklahoma',
+      'Arkansas',
+      'Louisiana',
+      'Alabama',
+      'California',
+      'South Carolina',
+    ],
     datasets: [
       {
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3, 12],
+        label: 'Population Estimates',
+        data: [
+          2600000, 400000, 500000, 500000, 1050000, 200000, 700000, 225000,
+          300000, 150000,
+        ],
         borderWidth: 1,
       },
     ],
